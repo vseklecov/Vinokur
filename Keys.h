68ed22583e3c8345c7fbe344dbab5391b0d3ffe1
Victor Sekletcov <sekletcov@gmail.com>
2018-02-01 10:56:30 +0300

    Версия

---
+++ Keys.h
@@ -0,0 +1,27 @@
+#ifndef __KEYS_H__
+#define __KEYS_H__
+
+const char KEY_NONE = '\0';
+const char KEY_ON = '@';
+const char KEY_MODE = 'M';
+const char KEY_SOUND = '\e';
+const char KEY_PLAY = 'P';
+const char KEY_PREV = 'S';
+const char KEY_NEXT = 'N';
+const char KEY_EQ = 'Q';
+const char KEY_MINUS = '-';
+const char KEY_PLUS = '+';
+const char KEY_ARROW = '\b';
+const char KEY_U_SD = '\n';
+const char KEY_0 = '0';
+const char KEY_1 = '1';
+const char KEY_2 = '2';
+const char KEY_3 = '3';
+const char KEY_4 = '4';
+const char KEY_5 = '5';
+const char KEY_6 = '6';
+const char KEY_7 = '7';
+const char KEY_8 = '8';
+const char KEY_9 = '9';
+
+#endif
